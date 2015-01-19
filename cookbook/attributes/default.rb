7be85864edb063559f5ebf4dc8d0f8a5a0e7f671
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-19 16:10:33 +0000

    Configure rabbitmq

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -1,8 +1,11 @@
-default['mariadb']['root_password']  = ''
-default['mariadb']['bind_address']  = ''
-
 default['openstack']['repos']  = [
   'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm',
   'http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm'
 ]
+
+default['mariadb']['root_password']  = ''
+default['mariadb']['bind_address']  = ''
+  
+default['rabbitmq']['user'] = ''
+default['rabbitmq']['password'] = ''
 
--- cookbook/recipes/rabbitmq.rb
+++ cookbook/recipes/rabbitmq.rb
@@ -1,3 +1,29 @@
 package 'rabbitmq-server' do
   action :upgrade
+  notifies :enable, 'service[rabbitmq]', :immediately
+  notifies :start, 'service[rabbitmq]', :immediately
+  notifies :run, 'execute[init user]', :immediately
+end
+
+execute 'init user' do
+  # Add sensitive true when foodcritic #233 fixed
+  command 'rabbitmqctl delete_user guest &&\
+    rabbitmqctl add_user ' + node['rabbitmq']['user'] + ' ' + node['rabbitmq']['password'] + ' &&\
+    rabbitmqctl set_permissions -p / ' + node['rabbitmq']['user'] + ' ".*" ".*" ".*"'
+  action :nothing
+  not_if { node['rabbitmq']['user'].empty? }
+end
+
+template '/etc/rabbitmq/rabbitmq-env.conf' do
+  source 'rabbitmq-env.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :restart, 'service[rabbitmq]', :immediately
+end
+
+service 'rabbitmq' do
+  name 'rabbitmq-server'
+  supports restart: true
+  action :nothing
 end
