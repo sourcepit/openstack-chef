ec6971df1eaf76e6a5a4b94161b59f0345d7eeea
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-19 11:42:40 +0000

    Use default rpm repo and mariadb 10.1.x

--- cookbook/recipes/mariadb.rb
+++ cookbook/recipes/mariadb.rb
@@ -1,4 +1,6 @@
 node.override['mariadb']['server_root_password'] = node['openstack']['mariadb']['root_password']
 node.override['mariadb']['allow_root_pass_change'] = node['openstack']['mariadb']['allow_root_pass_change']
+node.override['mariadb']['install']['version'] = '10.1'
+node.override['mariadb']['use_default_repository'] = true
 
 include_recipe "mariadb::server"
