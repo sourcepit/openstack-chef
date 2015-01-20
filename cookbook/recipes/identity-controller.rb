3451d0c18fc357282ea75c8f22010d9c61451800
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-20 13:20:48 +0000

    Use db user from attributes

--- cookbook/recipes/identity-controller.rb
+++ cookbook/recipes/identity-controller.rb
@@ -1,5 +1,5 @@
 execute 'create_db' do
-  command create_create_db_cmd(node['mariadb']['root_password'], "keystone", "keystone", "keystone")
+  command create_create_db_cmd(node['mariadb']['root_password'], "keystone", node['openstack']['keystone']['db']['user'], node['openstack']['keystone']['db']['password'])
   action :run
   notifies :restart, 'service[mariadb]', :immediately
 end
