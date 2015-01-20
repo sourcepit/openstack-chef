1299c9aa8f9264e8b3c8a2aca5ec84e5b5d4d590
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-20 10:16:49 +0000

    helpers.rb

---
+++ cookbook/lipraries/helpers.rb
@@ -0,0 +1,19 @@
+module Openstack
+  module DBHelper
+    def create_create_db_cmd(db_root_pass, db_name, db_user, db_pass)
+      sql = "mysql -u root "
+      unless db_root_pass.nil? or db_root_pass.empty?
+        sql += "-p" + db_root_pass + " "
+      end
+      sql += "-e \""
+      sql += "CREATE DATABASE " + db_name + ";\n"
+      sql += "GRANT ALL PRIVILEGES ON " + db_name + ".* TO '" + db_user + "@\'localhost\' IDENTIFIED BY '" + db_pass + ";\n"
+      sql += "GRANT ALL PRIVILEGES ON " + db_name + ".* TO '" + db_user + "@'%' IDENTIFIED BY '" + db_pass + "';"
+      sql += "\""
+    end
+  end
+end
+
+#Chef::Recipe.send(:include, ::Openstack::Helpers)
+#Chef::Resource.send(:include, ::Openstack::Helpers)
+#Chef::Provider.send(:include, ::Openstack::Helpers)
--- cookbook/recipes/identity-controller.rb
+++ cookbook/recipes/identity-controller.rb
@@ -1,17 +1,5 @@
-def create_create_db_cmd(db_root_pass, db_name, db_user, db_pass)
-  sql = "mysql -u root "
-  unless db_root_pass.nil? or db_root_pass.empty?
-    sql += "-p" + db_root_pass + " "
-  end
-  sql += "-e \""
-  sql += "CREATE DATABASE " + db_name + ";\n"
-  sql += "GRANT ALL PRIVILEGES ON " + db_name + ".* TO '" + db_user + "@\'localhost\' IDENTIFIED BY '" + db_pass + ";\n"
-  sql += "GRANT ALL PRIVILEGES ON " + db_name + ".* TO '" + db_user + "@'%' IDENTIFIED BY '" + db_pass + "';"
-  sql += "\""
-end
-
 execute 'create_db' do
-  command create_create_db_cmd(node['mariadb']['root_password'], "keystone", "keystone", "keystone")
+  command Openstack::DBHelper.create_create_db_cmd(node['mariadb']['root_password'], "keystone", "keystone", "keystone")
   action :run
   notifies :restart, 'service[mariadb]', :immediately
 end
