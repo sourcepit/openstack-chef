69190e9c6066397ff7a368bfdd068383febed675
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-21 08:42:23 +0100

    Test recipe global variables

--- cookbook/recipes/test.rb
+++ cookbook/recipes/test.rb
@@ -1,11 +1,11 @@
-auth_uri "http://#{node['openstack']['controller']['host']}:35357/v2.0"
+auth_uri = "http://#{node['openstack']['controller']['host']}:35357/v2.0"
 
-admin_tenant node['openstack']['admin']['tenant']
-admin_user node['openstack']['admin']['user']
-admin_password node['openstack']['admin']['password']
+admin_tenant = node['openstack']['admin']['tenant']
+admin_user = node['openstack']['admin']['user']
+admin_password = node['openstack']['admin']['password']
 
-service_user node['openstack']['image']['service']['user']
-service_password node['openstack']['image']['service']['password']
+service_user = node['openstack']['image']['service']['user']
+service_password = node['openstack']['image']['service']['password']
 
 openstack_identity "create service user '#{service_user}'" do
   auth_uri auth_uri
