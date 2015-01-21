8ba5615bc5c79c73ffeff373949f8cb23f55e1b5
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-21 15:30:16 +0100

    yum epel

--- cookbook/recipes/common.rb
+++ cookbook/recipes/common.rb
@@ -1,15 +1,10 @@
 
+include_recipe 'yum-epel::default'
+
 yum_repository 'rdo-release-juno' do
   description "OpenStack Juno Repository"
   baseurl 'http://repos.fedorapeople.org/repos/openstack/openstack-juno/epel-7/'
   gpgkey 'https://raw.githubusercontent.com/redhat-openstack/rdo-release/juno-1/RPM-GPG-KEY-RDO-Juno'
-  action :create
-end
-
-yum_repository 'epel-7' do
-  description 'Extra Packages for Enterprise Linux'
-  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-7'
-  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7'
   action :create
 end
 
