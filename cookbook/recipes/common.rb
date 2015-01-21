2a34e90aef6291f42af75e810326ea3f7ce3e5c4
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-21 15:15:35 +0100

    Fixed repo id

--- cookbook/recipes/common.rb
+++ cookbook/recipes/common.rb
@@ -1,5 +1,5 @@
 
-yum_repository 'OpenStack Juno' do
+yum_repository 'rdo-release-juno' do
   description "OpenStack Juno Repository"
   baseurl 'http://repos.fedorapeople.org/repos/openstack/openstack-juno/epel-7/'
   gpgkey 'https://raw.githubusercontent.com/redhat-openstack/rdo-release/juno-1/RPM-GPG-KEY-RDO-Juno'
