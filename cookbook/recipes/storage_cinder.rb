cfdb87bc79ebfbe05f6b91051fbbabc3ba5d0876
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-26 16:43:54 +0100

    fixed missing packages

--- cookbook/recipes/storage_cinder.rb
+++ cookbook/recipes/storage_cinder.rb
@@ -1,3 +1,9 @@
+%w(lvm2 openstack-cinder targetcli python-oslo-db MySQL-python).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
 include_recipe 'openstack::internal_cinder-conf'
 
 service 'openstack-cinder-volume' do
