1a060840b12cc261d4142fd875d2489eec1c363a
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-26 16:39:25 +0100

    don't migrate db here

--- cookbook/recipes/storage_cinder.rb
+++ cookbook/recipes/storage_cinder.rb
@@ -1,10 +1,5 @@
 include_recipe 'openstack::internal_cinder-conf'
 
-execute 'sync volume db' do
-  command 'su -s /bin/sh -c "cinder-manage db sync" cinder'
-  action :run
-end
-
 service 'openstack-cinder-volume' do
   supports status: true, restart: true
   action [:enable, :start]
