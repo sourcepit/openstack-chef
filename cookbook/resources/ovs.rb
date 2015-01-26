b53ec3d3a649282d797e27f163ab81e4adce44ed
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-01-26 09:42:26 +0100

    create ovs bridge

--- cookbook/resources/ovs.rb
+++ cookbook/resources/ovs.rb
@@ -1,29 +1,8 @@
-action :add_br do
+actions :add_br, :add_port
+default_action :add_br
 
-  cmd = Mixlib::ShellOut.new("ovs-vsctl br-exists #{new_resource.bridge}")
-  cmd.run_command
-  cmd.error!
+# add_br
+attribute :bridge, kind_of: String
 
-  if (cmd.stdout.empty?)
-    cmd = Mixlib::ShellOut.new("ovs-vsctl add-br #{new_resource.bridge}")
-    cmd.run_command
-    cmd.error!
-    new_resource.updated_by_last_action(true)
-  end
-
-end
-
-action :add_port do
-
-  cmd = Mixlib::ShellOut.new("ovs-vsctl list-ports #{new_resource.bridge} | grep #{new_resource.port}")
-  cmd.run_command
-  cmd.error!
-
-  if (cmd.stdout.empty?)
-    cmd = Mixlib::ShellOut.new("ovs-vsctl add-port #{new_resource.bridge} #{new_resource.port}")
-    cmd.run_command
-    cmd.error!
-    new_resource.updated_by_last_action(true)
-  end
-
-end
+# add_port
+attribute :port, kind_of: String
